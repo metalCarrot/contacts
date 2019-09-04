bf383001dd1911faa5f5060824fe84b8f75f3217
darkwoolf <woolftreuser@gmail.com>
2019-09-04 21:47:46 +0300

    init repository, added contact form, included files

---
+++ main.js
@@ -0,0 +1,9 @@
+let element = document.getElementById('create');
+
+element.onclick = function () {
+	// 1. Make validation
+	
+	// 2. get value from inputs
+	// 3. Add new contact to down
+	// 4. (not necessary) save to local storage
+}
